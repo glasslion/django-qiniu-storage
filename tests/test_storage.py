09d8c40890aefc3cb2210f458e6a4e80efb7097c
Leonardo.Z <glasslion@gmail.com>
2014-11-22 02:09:43 +0800

    Add a few tests about file add, write, read, stat...

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
 import os
 from os.path import dirname,join
 import unittest
@@ -31,7 +33,7 @@ class QiniuStorageTest(unittest.TestCase):
             self.file = QiniuFile('foo', self.storage, mode='rb')
             self.file.write("goto fail")
 
-    def test_write_file(self):
+    def test_write_and_delete_file(self):
         ASSET_FILE_NAME = 'jquery-1.11.1.min.js'
         assert self.storage.exists(ASSET_FILE_NAME) == False
         self.file = QiniuFile(ASSET_FILE_NAME, self.storage, mode='wb')
@@ -49,5 +51,42 @@ class QiniuStorageTest(unittest.TestCase):
 
         assert self.storage.size(ASSET_FILE_NAME) == assset_file_size
 
+        time_delta = datetime.now() - self.storage.modified_time(ASSET_FILE_NAME)
+        assert time_delta.total_seconds() < 60
+
         self.storage.delete(ASSET_FILE_NAME)
         assert self.storage.exists(ASSET_FILE_NAME) == False
+
+    def test_read_file(self):
+        ASSET_FILE_NAME = 'bootstrap.min.css'
+
+        with open(join(dirname(__file__),'assets', ASSET_FILE_NAME), 'rb') as assset_file:
+            self.storage.save(ASSET_FILE_NAME, assset_file)
+
+        self.file = self.storage.open(ASSET_FILE_NAME, 'r')
+
+        assert self.file._is_read == False
+
+        content = self.file.read()
+        assert content.startswith("/*!")
+
+        assert self.file._is_read == True
+
+    def test_dirty_file(self):
+        ASSET_FILE_NAME = 'bootstrap.min.css'
+
+        self.file = self.storage.open(ASSET_FILE_NAME, 'rw')
+
+        assert self.file._is_read == False
+        assert self.file._is_dirty == False
+        assert self.storage.exists(ASSET_FILE_NAME) == False
+
+        with open(join(dirname(__file__),'assets', ASSET_FILE_NAME), 'r') as assset_file:
+            content = assset_file.read()
+            self.file.write(content)
+
+        assert self.file._is_read == True
+        assert self.file._is_dirty == True
+
+        self.file.close()
+        assert self.storage.exists(ASSET_FILE_NAME) == True
