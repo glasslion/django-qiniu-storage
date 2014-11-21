e4cae2cbd5e9c19f723198ccb32b9cd8c4d8f9b7
Leonardo.Z <glasslion@gmail.com>
2014-11-21 14:00:02 +0800

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with codecs.open('DESCRIPTION.rst', encoding='utf-8') as f:
 
 setup(
     name="django-qiniu-storage",
-    version='1.0.0',
+    version='1.0.1',
     description="Django storage for Qiniu Cloud Storage",
     long_description=long_description,
 
