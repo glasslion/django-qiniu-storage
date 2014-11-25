b1b646736d84dca627f11b797dba6f660edc6086
Leonardo.Z <glasslion@gmail.com>
2014-11-25 09:28:39 +0800

    Remove debug info

--- qiniustorage/utils.py
+++ qiniustorage/utils.py
@@ -9,9 +9,8 @@ def bucket_lister(manager, bucket_name, prefix=None, marker=None, limit=None):
     """
     eof = False 
     while not eof:
-        ret, eof, info = manager.list(bucket_name, prefix=prefix, limit=2,
+        ret, eof, info = manager.list(bucket_name, prefix=prefix, limit=limit,
                                     marker=marker)
-        import pdb; pdb.set_trace()
         if ret is None:
             raise IOError("Failed to list bucket '%s'. "
                           "Error message: %s" % (bucket_name, err))
