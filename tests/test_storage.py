3289f7d519cb11c476842cbea0a5840801e3bc39
Leo Zhou <glasslion@gmail.com>
2015-12-29 13:00:42 +0800

    Remove large assets and use small files

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -9,7 +9,7 @@ import unittest
 import uuid
 
 import logging
-LOGGING_FORMAT = '\n%(asctime)-15s %(message)s'
+LOGGING_FORMAT = '\n%(levelname)s %(asctime)s %(message)s'
 logging.basicConfig(level=logging.INFO, format=LOGGING_FORMAT)
 logger = logging.getLogger(__name__)
 
@@ -51,7 +51,6 @@ if USING_TRAVIS:
 UNIQUE_PATH = str(uuid.uuid4())
 
 
-
 class QiniuStorageTest(unittest.TestCase):
     def setUp(self):
         self.storage = QiniuStorage()
@@ -67,24 +66,27 @@ class QiniuStorageTest(unittest.TestCase):
             fil.write("goto fail")
 
     def test_write_and_delete_file(self):
-        ASSET_FILE_NAMES = [u'jquery-2.1.1.min.js', u'jquery-2点壹点一.min.js']
+        ASSET_FILE_NAMES = [u'Write&Deltete.txt', u'写和删.txt']
         for assert_file_name in ASSET_FILE_NAMES:
             REMOTE_PATH = join(UNIQUE_PATH, assert_file_name)
+
             assert self.storage.exists(REMOTE_PATH) == False
             fil = QiniuFile(REMOTE_PATH, self.storage, mode='wb')
 
-            with open(join(dirname(__file__),'assets', assert_file_name), 'rb') as assset_file:
-                content = assset_file.read()
+            content = u"你好世界 Hello World"
 
-                assset_file.seek(0, os.SEEK_END)
-                assset_file_size = assset_file.tell()
+            # get file size
+            dummy_file = six.BytesIO()
+            dummy_file.write(content.encode('utf-8'))
+            dummy_file.seek(0, os.SEEK_END)
+            file_size = dummy_file.tell()
 
-                fil.write(content)
-                self.storage._save(REMOTE_PATH, fil)
+            fil.write(content)
+            self.storage._save(REMOTE_PATH, fil)
 
-                assert self.storage.exists(REMOTE_PATH)
+            assert self.storage.exists(REMOTE_PATH)
 
-            assert self.storage.size(REMOTE_PATH) == assset_file_size
+            assert self.storage.size(REMOTE_PATH) == file_size
 
             now = datetime.utcnow()
             modified_time = self.storage.modified_time(REMOTE_PATH)
@@ -99,29 +101,31 @@ class QiniuStorageTest(unittest.TestCase):
 
     @retry(ConnectionError,tries=10, backoff=1, logger=logger)
     def test_read_file(self):
-        ASSET_FILE_NAMES =  [u'jquery-1.11.1.min.js', u'jquery-一点十一点壹.js']
+        ASSET_FILE_NAMES =  [u'Read.txt', u'读.txt']
         for assert_file_name in ASSET_FILE_NAMES:
             REMOTE_PATH = join(UNIQUE_PATH, assert_file_name)
 
-            with open(join(dirname(__file__),'assets', assert_file_name), 'rb') as assset_file:
-                self.storage.save(REMOTE_PATH, assset_file)
+            test_file = six.BytesIO()
+            test_file.write(u"你好世界 Hello World".encode('utf-8'))
+            test_file.seek(0)
+            self.storage.save(REMOTE_PATH, test_file)
 
             fil = self.storage.open(REMOTE_PATH, 'r')
 
             assert fil._is_read == False
 
             content = fil.read()
-            assert content.startswith(u"/*!")
+            assert content.startswith(u"你好")
 
             assert fil._is_read == True
 
             # Test open mode
             fil = self.storage.open(REMOTE_PATH, 'rb')
             bin_content = fil.read()
-            assert bin_content.startswith(b"/*!")
+            assert bin_content.startswith(u"你好".encode('utf-8'))
 
     def test_dirty_file(self):
-        ASSET_FILE_NAME = 'bootstrap.min.css'
+        ASSET_FILE_NAME = u'测试脏文件.txt'
         REMOTE_PATH = join(UNIQUE_PATH, ASSET_FILE_NAME)
 
         fil = self.storage.open(REMOTE_PATH, 'rw')
@@ -130,9 +134,7 @@ class QiniuStorageTest(unittest.TestCase):
         assert fil._is_dirty == False
         assert self.storage.exists(REMOTE_PATH) == False
 
-        with open(join(dirname(__file__),'assets', ASSET_FILE_NAME), 'r') as assset_file:
-            content = assset_file.read()
-            fil.write(content)
+        fil.write("Hello World.")
 
         assert fil._is_read == True
         assert fil._is_dirty == True
