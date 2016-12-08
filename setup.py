728c68ed9013f07a102dd82cc045d567e7d6eb20
Smite Chow <xiaopengyou@live.com>
2016-12-08 15:59:59 +0800

    release 2.3.2

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ except IOError:
 
 setup(
     name="django-qiniu-storage",
-    version='2.3.1',
+    version='2.3.2',
     description="Django storage for Qiniu Cloud Storage",
     long_description=long_description,
 
