86b65be4b80f25dcb1a8ad1d8d150b338d4cf56c
Leo Zhou <glasslion@gmail.com>
2017-02-19 21:14:02 +0800

    lint setup.py

--- setup.py
+++ setup.py
@@ -6,25 +6,6 @@ import re
 if os.environ.get('USER','') == 'vagrant':
     del os.link
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Read the version number from a source file.
-# Why read it, and not import?
-# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
-def find_version(*file_paths):
-    # Open in Latin-1 so that we avoid encoding errors.
-    # Use codecs.open for Python 2 compatibility
-    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
-        version_file = f.read()
-
-    # The version line must have the form
-    # __version__ = 'ver'
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
 
 # Get the long description from the relevant file
 try:
